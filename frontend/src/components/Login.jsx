d3e47125e865fce36bc4630cbed41bb51cc0b04a
kapilkauushal24 <kapilkaushal466@gmail.com>
2024-10-21 21:05:30 +0530

    Your commit message here

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
             onClick={() => navigate("/signup")}
             className="text-blue-500 hover:underline"
           >
-            Sign Up
+            Register Now
           </button>
         </p>
       </div>
