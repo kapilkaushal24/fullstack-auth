5ac7f5492ec910d19732b87b4a59905a1b5f9957
kapilkauushal24 <kapilkaushal466@gmail.com>
2024-10-21 20:47:03 +0530

    first commit to main branch

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,6 @@ from .views import RegisterView,home, LoginView,IsAuthenticated
 
 urlpatterns = [
     path('', home, name='home'), 
-    # path('', IsAuthenticated.as),
     path('register', RegisterView.as_view(), name='register'),
     path('login', LoginView.as_view(), name='login')
 ]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Home" element={<Home/>}/>
+        <Route path="/navbar" element={<Navbar/>}/>
       </Routes>
     </>
   );
